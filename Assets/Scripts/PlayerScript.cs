293950a53b471709395c1868e08b5d8bb607ec07
AnicetoB <aniceto.bokari.egea@gmail.com>
2014-11-05 18:36:06 +0100

    Tarea 01 - Movimiento del personaje
    Añadido movimiento del personaje sin animación, con arreglo de collider
    redondo en pies para subir escalones.
    
    Tarea 02 - Añadir animación personaje

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	public float speed = 10f;
+	public Vector2 maxVelocity = new Vector2(2,3);
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		var absVelX = Mathf.Abs (rigidbody2D.velocity.x);
+		var forceX = 0f;
+		var forceY = 0f;
+
+		if (Input.GetKey ("right")) {
+			if (absVelX < maxVelocity.x)
+				forceX = speed;
+			this.transform.localScale = new Vector3 (1, 1, 1);
+
+		}else if (Input.GetKey ("left")) {
+			if (absVelX < maxVelocity.x)
+				forceX = -speed;
+			this.transform.localScale = new Vector3 (-1, 1, 1);
+		}
+		rigidbody2D.AddForce(new Vector2 (forceX, forceY));
+	}
+}
