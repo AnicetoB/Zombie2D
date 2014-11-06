f2daa6bf281fef887d14780f8d35fc867d0ab058
AnicetoB <aniceto.bokari.egea@gmail.com>
2014-11-06 19:28:15 +0100

    Tarea 03 - Animación solucionada y disparar iniciado

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -3,8 +3,13 @@ using System.Collections;
 
 public class PlayerScript : MonoBehaviour {
 
+	public GameObject proyectil;
 	public float speed = 10f;
 	public Vector2 maxVelocity = new Vector2(2,3);
+	public float PosicionBalaX = 1;
+	public float PosicionBalaY = 1;
+	public bool mirarderecha = true;
+
 	Animator anim;
 	// Use this for initialization
 	void Start () {
@@ -18,18 +23,26 @@ public class PlayerScript : MonoBehaviour {
 		var forceX = 0f;
 		var forceY = 0f;
 
+
 		if (Input.GetKey ("right")) {
+			mirarderecha = true;
 			if (absVelX < maxVelocity.x)
 				forceX = speed;
 			this.transform.localScale = new Vector3 (1, 1, 1);
 			anim.SetBool("andar",true); 
 
 		}else if (Input.GetKey ("left")) {
+			mirarderecha = false;
 			if (absVelX < maxVelocity.x)
 				forceX = -speed;
 			this.transform.localScale = new Vector3 (-1, 1, 1);
 			anim.SetBool("andar",true); 
+
+		}else if (Input.GetKeyUp ("left") || Input.GetKeyUp ("right")){
+			anim.SetBool("andar",false); 
+		}else if (Input.GetKeyDown ("space")) {
+				var proy = (GameObject)Instantiate(proyectil, new Vector3 (PosicionBalaX, PosicionBalaY,0),transform.rotation);
 		}
-			rigidbody2D.AddForce(new Vector2 (forceX, forceY));
-		}
+		rigidbody2D.AddForce(new Vector2 (forceX, forceY));
 	}
+}
---
+++ Assets/Scripts/cameraFollow.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraFollow : MonoBehaviour {
+	
+	public Transform target; 						//esta variable sirve para adaptar el componente transform de la camara al del personaje, cogiendo sus tres ejes
+	public float damping = 1; 						//esta variable sirve para amortiguar el seguimiento de la camara en el valor que se le asigne
+	public float lookAheadFactor = 3; 				//
+	public float lookAheadReturnSpeed = 0.5f; 		//esta variable servira para 
+	public float lookAheadMoveThreshold = 0.1f; 	//
+	public float yPosRestriction = -1; 				//restringe la posicion y de la camara
+	
+	float offsetZ;					//crea una variable para la compensacion del eje Z
+	Vector3 lastTargetPosition;		//crea un vector de 3 ejes al que se le asignara la posicion primitiva del target
+	Vector3 currentVelocity;		//crea un vector de 3 ejes al que se le asignara la velocidad actual de la camara
+	Vector3 lookAheadPos;			//crea un vector de 3 ejes al que se le asignara la posicion de cuando el personaje va hacia delante
+
+	void Start () {
+		lastTargetPosition = target.position;		//esta linea le da a la variable utilizada la posicion actual del target
+		offsetZ = (transform.position - target.position).z;		
+		//se le da a la variable offsetZ un valor que cambia la posicion del eje z, de forma que la camara no este en el mismo plano que el escenario
+		transform.parent = null;		//??
+	}
+
+	void Update () {
+		
+		// only update lookahead pos if accelerating or changed direction
+		float xMoveDelta = (target.position - lastTargetPosition).x;		
+		
+		bool updateLookAheadTarget = Mathf.Abs(xMoveDelta) > lookAheadMoveThreshold;
+		
+		if (updateLookAheadTarget) {
+			lookAheadPos = lookAheadFactor * Vector3.right * Mathf.Sign(xMoveDelta);
+		} else {
+			lookAheadPos = Vector3.MoveTowards(lookAheadPos, Vector3.zero, Time.deltaTime * lookAheadReturnSpeed);	
+		}
+		
+		Vector3 aheadTargetPos = target.position + lookAheadPos + Vector3.forward * offsetZ;
+		Vector3 newPos = Vector3.SmoothDamp(transform.position, aheadTargetPos, ref currentVelocity, damping);
+		
+		newPos = new Vector3(newPos.x, Mathf.Clamp(newPos.y,yPosRestriction,Mathf.Infinity), newPos.z);
+		transform.position = newPos;
+		
+		lastTargetPosition = target.position;		
+	}
+}
